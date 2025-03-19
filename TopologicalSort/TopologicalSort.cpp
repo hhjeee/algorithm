11946408c9fb380627284c43db4c99afd4e24f32
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-03-19 22:37:45 +0900

    Update TopologicalSort.cpp

--- TopologicalSort/TopologicalSort.cpp
+++ TopologicalSort/TopologicalSort.cpp
@@ -1,30 +1,52 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <vector>
 using namespace std;
 
 int n; // #node
-vector<vector<int>> adjList(n + 1);
-vector<int> indegree(n + 1, 0); //입력받을때 간선받는 노드의 indegree++;
+vector<vector<int>> adjList;
+vector<int> indegree; // 입력받을때 간선받는 노드의 indegree++;
 
 void topologicalSort() {
-	queue<int> q;
+  queue<int> q;
 
-	for (int i = 1; i <= n; i++) {
-		if (!indegree[i]) q.push(i);
-	}
+  for (int i = 1; i <= n; i++) {
+    if (!indegree[i])
+      q.push(i);
+  }
 
-	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
+  while (!q.empty()) {
+    int cur = q.front();
+    q.pop();
 
-		for (int next : adjList[cur]) {
-			indegree[next]--;
+    for (int next : adjList[cur]) {
+      indegree[next]--;
 
-			if (indegree[next] == 0) q.push(next);
-		}
+      if (indegree[next] == 0)
+        q.push(next);
+    }
 
-		cout << cur << ' ';
-	}
+    cout << cur << ' ';
+  }
+}
+
+int main() {
+  int m;
+  cin >> n >> m;
+
+  adjList.resize(n + 1);
+  indegree.resize(n + 1, 0);
+
+  while (m--) {
+    int a, b;
+    cin >> a >> b;
+
+    adjList[a].push_back(b);
+    indegree[b]++;
+  }
+
+  topologicalSort();
+
+  return 0;
 }
