f350c6cde7c24af02b06fdeee17a224dc0615162
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-03-21 01:25:41 +0900

    Update FloydWarshall.cpp

--- 최단거리/FloydWarhsall/FloydWarshall.cpp
+++ 최단거리/FloydWarhsall/FloydWarshall.cpp
@@ -1,30 +1,42 @@
-#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <vector>
-#include <climits>
 using namespace std;
 
-int n; // #node
-vector<vector<int>> dist(n + 1, vector<int>(n + 1, INT_MAX));
+int n, m; // #node, #edge
+vector<vector<int>> dist;
 
-void init() {
-	for (int i = 1; i <= n; i++) {
-		dist[i][i] = 0;
-	}
-
-	// a -> b 일때의 cost(c) 목록 받아서
-	// dist[a][b] = c;
-}
 void FloydWarshall() {
-	for (int k = 1; k <= n; k++) {
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= n; j++) {
-				if (i == k || k == i) continue;
-				if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX) continue;
+  for (int k = 1; k <= n; k++) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (i == k || k == i)
+          continue;
+        if (dist[i][k] == INT_MAX || dist[k][j] == INT_MAX)
+          continue;
 
-				if (dist[i][j] > dist[i][k] + dist[k][i])
-					dist[i][j] = dist[i][k] + dist[k][i];
-			}
-		}
-	}
+        if (dist[i][j] > dist[i][k] + dist[k][j])
+          dist[i][j] = dist[i][k] + dist[k][j];
+      }
+    }
+  }
+}
+
+int main() {
+  cin >> n >> m;
+
+  dist.assign(n + 1, vector<int>(n + 1, INT_MAX));
+
+  for (int i = 1; i <= n; i++)
+    dist[i][i] = 0;
+
+  for (int i = 0; i < m; i++) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    dist[u][v] = w;
+  }
+
+  FloydWarshall();
+
+  return 0;
 }
