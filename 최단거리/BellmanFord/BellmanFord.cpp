5d6705ad3a4f19adf855d4e2f3990f93d2cc3c39
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-03-21 01:04:57 +0900

    Update BellmanFord.cpp

--- 최단거리/BellmanFord/BellmanFord.cpp
+++ 최단거리/BellmanFord/BellmanFord.cpp
@@ -1,44 +1,59 @@
-#include <algorithm>
+#include <climits>
 #include <iostream>
 #include <vector>
-#include <climits>
 using namespace std;
 
 struct edge {
-	int from;
-	int to;
-	int cost;
+  int from, to, cost;
 };
 
-int v, e; 
+int v, e;
+vector<long long int> dist;
 vector<edge> edgeList;
-vector<long long int> dist(v + 1, LONG_MAX);
 
-bool BellmanFord(int start) {
-	dist[start] = 0;
+bool bellmanFord(int start) {
+  dist[start] = 0;
 
-	//v-1번 탐색
-	for (int i = 0; i < v - 1; i++) {
-		for (edge e : edgeList) {
-			//간선의 시작점이 탐색불가
-			if (dist[e.from] == LONG_MAX) continue;
+  // v-1번 탐색
+  for (int i = 0; i < v - 1; i++) {
+    for (edge e : edgeList) {
+      // 간선의 시작점이 탐색불가
+      if (dist[e.from] == LLONG_MAX)
+        continue;
 
-			if (dist[e.to] > dist[e.from] + dist[e.cost])
-				dist[e.to] = dist[e.from] + dist[e.cost];
-		}
-	}
+      if (dist[e.to] > dist[e.from] + e.cost) {
+        dist[e.to] = dist[e.from] + e.cost;
+      }
+    }
+  }
 
-	//음의 사이클 검사
-	bool negativeCycle = false;
+  // 음의 사이클 검사
+  for (edge e : edgeList) {
+    if (dist[e.from] == LLONG_MAX)
+      continue;
 
-	for (edge e : edgeList) {
-		if (dist[e.from] == LONG_MAX) continue;
+    if (dist[e.to] > dist[e.from] + e.cost) {
+      return true;
+    }
+  }
 
-		if (dist[e.to] > dist[e.from] + dist[e.cost]) {
-			negativeCycle = true;
-			break;
-		}
-	}
+  return false;
+}
 
-	return negativeCycle;
+int main() {
+  cin >> v >> e;
+
+  dist.resize(v + 1, LLONG_MAX);
+  edgeList.resize(e);
+
+  for (int i = 0; i < e; i++) {
+    cin >> edgeList[i].from >> edgeList[i].to >> edgeList[i].cost;
+  }
+
+  int start;
+  cin >> start;
+
+  bool haveNegativeCycle = bellmanFord(start);
+
+  return 0;
 }
