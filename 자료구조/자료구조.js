f273c8dc4d8207783bb3842c42d592add2c272df
Hyunji Yun <96991702+hhjeee@users.noreply.github.com>
2025-03-21 16:17:27 +0900

    Update 자료구조.js

--- 자료구조/자료구조.js
+++ 자료구조/자료구조.js
@@ -65,20 +65,64 @@ class PriorityQueue {
     constructor() {
         this.arr = [];
     }
-    push(a){
+    push(a) {
         this.arr.push(a);
-        this.arr.sort((a,b) => a-b);
+        this.heapifyUp();
     }
-    pop(){
-        this.arr.shift();
+    pop() {
+        if (this.arr.length === 0) return null;
+        if (this.arr.length === 1) return this.arr.pop();
+        
+        let top = this.arr[0];
+        this.arr[0] = this.arr.pop();
+        this.heapifyDown();
+        return top;
     }
-    front(){
+    top() {
         return this.arr[0];
     }
-    empty(){
-        return this.arr.length === 0; 
+    size() {
+        return this.arr.length;
+    }
+    heapifyUp() {
+        let index = this.arr.length - 1;
+         
+        while (index > 0) {
+            let parent = Math.floor((index - 1) / 2);
+            if (this.arr[parent] <= this.arr[index]) break;
+            
+            [this.arr[index], this.arr[parent]] =
+                [this.arr[parent], this.arr[index]];
+            
+            index = parent;
+        }
+    }
+    heapifyDown() {
+        let index = 0;
+        let length = this.arr.length;
+        
+        while(1) {
+            let left_child = index * 2 + 1;
+            let right_child = index * 2 + 2;
+            let smallest = index;
+            
+            if(left_child < length &&
+               this.arr[left_child] < this.arr[smallest])
+                smallest = left_child;
+            if(right_child < length &&
+               this.arr[right_child] < this.arr[smallest])
+                smallest = right_child;
+            
+            if(index == smallest) break;
+            
+            [this.arr[index], this.arr[smallest]] = 
+                [this.arr[smallest], this.arr[index]];
+            
+            index = smallest;
+        }
     }
 }
+
 
 // map
 
